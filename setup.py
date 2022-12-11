1ce418194f7028d1b6b64e18045f6510532212d1
Estasleyendoesto <alsival@hotmail.com>
2022-12-11 20:23:56 +0100

    setup OK

--- setup.py
+++ setup.py
@@ -46,8 +46,6 @@ class Setup_utils_panel(bpy.types.Panel):
         row = layout.row()
         row.operator("object.reset_all")
 
-        
-
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
 # Operators
@@ -58,7 +56,7 @@ class FigureLoader(bpy.types.Operator):
 
     def execute(self, context):
         import os
-        path = os.path.dirname(os.path.abspath(__file__)) + r'\base.blend'
+        path = os.path.dirname(os.path.abspath(__file__)) + r'\artisan.blend'
         
         objects_to_append = ['Genesis 9']
         with bpy.data.libraries.load(path) as (data_from, data_to):
@@ -66,7 +64,6 @@ class FigureLoader(bpy.types.Operator):
 
         D = bpy.data
         C = bpy.context
-
         coll_name = 'Workspace'
         coll = C.scene.collection.children.get(coll_name)
         if coll is None:
@@ -77,6 +74,8 @@ class FigureLoader(bpy.types.Operator):
         for obj in data_to.objects:
             coll.objects.link(obj)
 
+        bpy.data.libraries.remove(bpy.data.libraries.get('artisan.blend'))
+
         return {'FINISHED'}
 
 class SceneLoader(bpy.types.Operator):
@@ -85,11 +84,17 @@ class SceneLoader(bpy.types.Operator):
 
     def execute(self, context):
         import os
-        path = os.path.dirname(os.path.abspath(__file__)) + r'\base.blend'
+        path = os.path.dirname(os.path.abspath(__file__)) + r'\artisan.blend'
 
-        to_append = ['Artisan']
+        D = bpy.data
+        C = bpy.context
         with bpy.data.libraries.load(path) as (data_from, data_to):
-            data_to.scenes = [scene for scene in data_from.scenes if scene in to_append]
+            data_to.collections = [c for c in data_from.collections if c != 'GENESIS 9 FIGURE']
+
+        for c in data_to.collections:
+            C.scene.collection.children.link(c)
+        
+        bpy.data.libraries.remove(bpy.data.libraries.get('artisan.blend'))
 
         return {'FINISHED'}
 
@@ -98,7 +103,30 @@ class RenderSettingsLoader(bpy.types.Operator):
     bl_label = 'Load Render Settings'
 
     def execute(self, context):
-        print('Render Settings loaded')
+        C = bpy.context
+
+        # Cycles
+        C.scene.cycles.preview_samples        = 512
+        C.scene.cycles.use_preview_denoising  = True
+        C.scene.cycles.preview_denoiser       = 'OPTIX'
+        C.scene.cycles.preview_denoising_input_passes = 'RGB_ALBEDO_NORMAL'
+        C.scene.cycles.samples                = 720
+        C.scene.cycles.use_denoising          = True
+        C.scene.cycles.denoiser               = 'OPTIX'
+        C.scene.cycles.denoising_input_passes = 'RGB_ALBEDO_NORMAL'
+        # Cycles - Light Paths
+        C.scene.cycles.max_bounces             = 6
+        C.scene.cycles.diffuse_bounces         = 2
+        C.scene.cycles.glossy_bounces          = 3
+        C.scene.cycles.transmission_bounces    = 6
+        C.scene.cycles.volume_bounces          = 1
+        C.scene.cycles.transparent_max_bounces = 32
+        # Color Management
+        C.scene.view_settings.view_transform    = 'Filmic'
+        C.scene.view_settings.look              = 'Medium Low Contrast'
+        C.scene.view_settings.exposure          = 0.4
+        C.scene.view_settings.gamma             = 0.8   
+        C.scene.view_settings.use_curve_mapping = True
 
         return {'FINISHED'}
 
@@ -107,7 +135,29 @@ class ResetAll(bpy.types.Operator):
     bl_label = 'Reset Default'
 
     def execute(self, context):
-        print('All resets')
+        C = bpy.context
+        # Cycles
+        C.scene.cycles.preview_samples        = 1024
+        C.scene.cycles.use_preview_denoising  = False
+        C.scene.cycles.preview_denoiser       = 'AUTO'
+        C.scene.cycles.preview_denoising_input_passes = 'RGB_ALBEDO'
+        C.scene.cycles.samples                = 4096
+        C.scene.cycles.use_denoising          = True
+        C.scene.cycles.denoiser               = 'OPENIMAGEDENOISE'
+        C.scene.cycles.denoising_input_passes = 'RGB_ALBEDO_NORMAL'
+        # Cycles - Light Paths
+        C.scene.cycles.max_bounces             = 12
+        C.scene.cycles.diffuse_bounces         = 4
+        C.scene.cycles.glossy_bounces          = 4
+        C.scene.cycles.transmission_bounces    = 12
+        C.scene.cycles.volume_bounces          = 0
+        C.scene.cycles.transparent_max_bounces = 8
+        # Color Management
+        C.scene.view_settings.view_transform    = 'Filmic'
+        C.scene.view_settings.look              = 'None'
+        C.scene.view_settings.exposure          = 0
+        C.scene.view_settings.gamma             = 1
+        C.scene.view_settings.use_curve_mapping = False
 
         return {'FINISHED'}
 
